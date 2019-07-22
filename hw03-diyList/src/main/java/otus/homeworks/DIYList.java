74ae1c68760ea7c8594063df455e2e31c874a273
Nonna <dz.nonna@gmail.com>
2019-07-22 14:54:26 -0700

    HW03 -  UnsupportedOperationException was added to unimplemented methods

--- hw03-diyList/src/main/java/otus/homeworks/DIYList.java
+++ hw03-diyList/src/main/java/otus/homeworks/DIYList.java
@@ -40,17 +40,17 @@ public class DIYList<T> extends DIYRandomAccess
 
   @Override
   public boolean isEmpty() {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public boolean contains(Object o) {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public Iterator<T> iterator() {
-    return null;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
@@ -60,7 +60,7 @@ public class DIYList<T> extends DIYRandomAccess
 
   @Override
   public <T1> T1[] toArray(T1[] a) {
-    return null;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
@@ -88,37 +88,37 @@ public class DIYList<T> extends DIYRandomAccess
 
   @Override
   public boolean remove(Object o) {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public boolean containsAll(Collection<?> c) {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public boolean addAll(Collection<? extends T> c) {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public boolean addAll(int index, Collection<? extends T> c) {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public boolean removeAll(Collection<?> c) {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public boolean retainAll(Collection<?> c) {
-    return false;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public void clear() {
-
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
@@ -128,26 +128,27 @@ public class DIYList<T> extends DIYRandomAccess
 
   @Override
   public T set(int index, T element) {
-    return null;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public void add(int index, T element) {
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public T remove(int index) {
-    return null;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public int indexOf(Object o) {
-    return 0;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public int lastIndexOf(Object o) {
-    return 0;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
@@ -157,11 +158,11 @@ public class DIYList<T> extends DIYRandomAccess
 
   @Override
   public ListIterator<T> listIterator(int index) {
-    return null;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 
   @Override
   public List<T> subList(int fromIndex, int toIndex) {
-    return null;
+    throw new UnsupportedOperationException("Method is not implemented");
   }
 }
--- hw03-diyList/src/main/java/otus/homeworks/DIYRandomAccess.java
+++ hw03-diyList/src/main/java/otus/homeworks/DIYRandomAccess.java
@@ -5,4 +5,5 @@ import java.util.RandomAccess;
 public class DIYRandomAccess implements RandomAccess {
 
   //This class was created to avoid 2) DIYarrayList должен имплементировать ТОЛЬКО ОДИН интерфейс - List.
+  //and make Collections.copy() usable for more than 10 elements
 }
